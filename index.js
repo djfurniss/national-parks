3471cc366f2fc9201c42c98156fc3b0316a27dfa
Denasia <dfurniss@ymail.com>
2022-08-04 17:22:04 -0400

    made it through the modifications of the module

--- index.js
+++ index.js
@@ -14,7 +14,63 @@ const submitHandler = (event) => {
     } else {
       document.querySelector(errorId).style.display = "none";
     }
+
+    const errors = validateForm(formData);
+
+  // Clear all previous errors
+  const errorElements = document.querySelectorAll(".error");
+  for (let element of errorElements) {
+    element.style.display = "none";
+  }
+
+  // Display any new errors
+  Object.keys(errors).forEach((key) => {
+    // Find the specific error element
+    const errorElement = document.querySelector(`#${key}-form .error`);
+    errorElement.innerHTML = errors[key];
+    errorElement.style.display = "block";
   });
+
+  // If there are no errors
+if (!Object.keys(errors).length) {
+    // Create a new element
+    const parkSection = document.createElement("section");
+  
+    // Add the park class
+    parkSection.classList.add("park-display");
+  
+    // Construct the HTML for this element
+    const content = `
+      <h2>${formData.get("name")}</h2>
+      <div class="location-display">${formData.get("location")}</div>
+      <div class="description-display">${formData.get("description")}</div>
+      <button class="rate-button" title="Add to Favourites">&#9734;</button>
+      <div class="stats">
+        <div class="established-display stat">
+          <h3>Established</h3>
+          <div class="value">${moment(formData.get("established")).format(
+            "MMMM D, YYYY"
+          )}</div>
+        </div>
+        <div class="area-display stat">
+          <h3>Area</h3>
+          <div class="value">${formData.get("area")}</div>
+        </div>
+        <div class="rating-display stat">
+          <h3>Rating</h3>
+          <div class="value">${formData.get("rating")}</div>
+        </div>
+      </div>
+      `;
+  
+    // Set the innerHTML
+    parkSection.innerHTML = content;
+  
+    // Append to the main element
+    document.querySelector("main").appendChild(parkSection);
+  }
+});
+
 
   // if there are no errors
   if (!hasErrors) {
@@ -132,6 +188,89 @@ const ratingSorterClickHandler = (event) => {
   parksArray.forEach((park) => {
     main.appendChild(park);
   });
+
+const renderOnePark = (park) => {
+    // Get the individual properties of the park
+    const { name, location, description, established, area, rating } = park;
+
+    const content = `
+        <section class="park-display">
+            <h2>${name}</h2>
+            <div class="location-display">${location}</div>
+            <div class="description-display">${description}</div>
+            <button class="rate-button" title="Add to Favourites">&#9734;</button>
+            <div class="stats">
+            <div class="established-display stat">
+                <h3>Established</h3>
+                <div class="value">${established}</div>
+            </div>
+            <div class="area-display stat">
+                <h3>Area</h3>
+                <div class="value">${area}</div>
+            </div>
+            <div class="rating-display stat">
+                <h3>Rating</h3>
+                <div class="value">${rating}</div>
+            </div>
+            </div>
+        </section>
+    `;
+    return content;
+};
+
+const render = () => {
+    // Get the parent element
+    const main = document.querySelector("main");
+
+    // Empty the parent element
+    main.innerHTML = "";
+
+    // Get the parks HTML
+    const content = parks.map(renderOnePark).join("");
+
+    // Set the `innerHTML` of parent element
+    main.innerHTML = content;
+};
+
+function validateExists(value) {
+    return value && value.trim();
+  }
+
+function validateForm(formData) {
+    const errors = {};
+
+    // Check if name was entered
+    if (!validateExists(formData.get("name"))) {
+        errors.name = "Please enter a name";
+    }
+
+    // Check if rating was entered
+    if (!validateExists(formData.get("rating"))) {
+        errors.rating = "Please enter a rating";
+    }
+
+    // Check if description was entered
+    if (!validateExists(formData.get("description"))) {
+        errors.description = "Please enter short description";
+    }
+
+    // Check if established date was entered
+    if (!validateExists(formData.get("established"))) {
+        errors.established = "Please enter date";
+    }
+
+    // Check if area was entered
+    if (!validateExists(formData.get("area"))) {
+        errors.area = "Please enter the area of the park";
+    }
+
+    // Check if location date was entered
+    if (!validateExists(formData.get("location"))) {
+        errors.location = "Please enter the location of the park";
+    }
+
+    return errors;
+}
 };
 
 // the point where all the code starts
@@ -154,6 +293,8 @@ const main = () => {
   // iterate the list of buttons and add an event handler to each
   allBtns.forEach((btn) => {
     btn.addEventListener("click", favoriteButtonClickHandler);
+
+    render();
   });
 
   // get the form element
