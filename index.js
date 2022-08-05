3d35d6cf6b3a95002f3181b811ea2cd4ba2fe041
Denasia <dfurniss@ymail.com>
2022-08-05 09:34:51 -0400

    finished with module

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+const renderOnePark = (park) => {
+    // Get the individual properties of the park
+    const { name, location, description, established, area, rating } = park;
+  
+    const content = `
+        <section class="park-display">
+          <h2>${name}</h2>
+          <div class="location-display">${location}</div>
+          <div class="description-display">${description}</div>
+          <button class="rate-button" title="Add to Favourites">&#9734;</button>
+          <div class="stats">
+            <div class="established-display stat">
+              <h3>Established</h3>
+              <div class="value">${established}</div>
+            </div>
+            <div class="area-display stat">
+              <h3>Area</h3>
+              <div class="value">${area}</div>
+            </div>
+            <div class="rating-display stat">
+              <h3>Rating</h3>
+              <div class="value">${rating}</div>
+            </div>
+          </div>
+        </section>
+    `;
+    return content;
+};
+
+const render = () => {
+    // Get the parent element
+    const main = document.querySelector("main");
+  
+    // Empty the parent element
+    main.innerHTML = "";
+  
+    // Get the parks HTML
+    const content = parks.map(renderOnePark).join("");
+  
+    // Set the `innerHTML` of parent element
+    main.innerHTML = content;
+};
+
 const submitHandler = (event) => {
     event.preventDefault();
   
@@ -18,52 +61,35 @@ const submitHandler = (event) => {
   
     // if there are no errors
     if (!hasErrors) {
-      //create a new element
-      const parkSection = document.createElement("section");
-  
-      // add the park class
-      parkSection.classList.add("park-display");
-  
-      // construct the HTML for this element
-      const content = `
-      <h2>${formData.get("name")}</h2>
-      <div class="location-display">${formData.get("location")}</div>
-      <div class="description-display">${formData.get("description")}</div>
-      <button class="rate-button" title="Add to Favourites">&#9734;</button>
-      <div class="stats">
-        <div class="established-display stat">
-          <h3>Established</h3>
-          <div class="value">${formData.get("established")}</div>
-        </div>
-        <div class="area-display stat">
-          <h3>Area</h3>
-          <div class="value">${formData.get("area")}</div>
-        </div>
-        <div class="rating-display stat">
-          <h3>Rating</h3>
-          <div class="value">${formData.get("rating")}</div>
-        </div>
-      </div>
-      `;
-  
-      // set the innerHTML
-      parkSection.innerHTML = content;
-  
-      //append to the main element
-      document.querySelector("main").appendChild(parkSection);
+        // Create an empty object with the input values
+        const park = {
+            name: formData.get("name"),
+            location: formData.get("location"),
+            description: formData.get("description"),
+            established: formData.get("established"),
+            area: formData.get("area"),
+            rating: formData.get("rating"),
+        };
+        //put the new park with the rest of the parks
+        parks.push(park);
+        //render the whole page again wtih the new data
+        render();
     }
 };
   
-  // function to handler favorite button clicks
+// function to handler favorite button clicks
 const favoriteButtonClickHandler = (event) => {
+    //now that the listener for this handler is on ALL of main, this if statement makes sure that a button was its target this is EVENT DELEGATION because they actual event happened way higher and outside of button, it happened in main but you were able to bring it out from button and let main's listener handle the button that's like 2 levels down in the DOM
+    if(event.target && event.target.nodeName == "BUTTON"){
     const park = event.target.parentNode;
     park.style.backgroundColor = "#c8e6c9";
-  };
+    };
+}
   
-  // function for sorting by name
-  const sortByName = (parkA, parkB) => {
-    const parkAName = parkA.querySelector("h2").innerText;
-    const parkBName = parkB.querySelector("h2").innerText;
+// function for sorting by name
+const sortByName = (parkA, parkB) => {
+    const parkAName = parkA.name;
+    const parkBName = parkB.name;
     if (parkAName < parkBName) {
       return -1;
     } else if (parkAName > parkBName) {
@@ -73,65 +99,31 @@ const favoriteButtonClickHandler = (event) => {
     }
 };
   
-  // function for sorting by rating
+// function for sorting by rating
 const sortByRating = (parkA, parkB) => {
-    const parkARating = parseFloat(
-      parkA.querySelector(".rating-display > .value").innerText
-    );
-    const parkBRating = parseFloat(
-      parkB.querySelector(".rating-display > .value").innerText
-    );
-    return parkBRating - parkARating;
+    // const parkARating = parkA.rating;
+    // const parkBRating = parkB.rating;
+    return parkB.rating - parkA.rating;
 };
   
-  // function for handling the nameSorter click
+// function for handling the nameSorter click
 const nameSorterClickHandler = (event) => {
     event.preventDefault();
+
+    // sort the array
+    parks.sort(sortByName);
   
-    // 1.  get the main element
-    const main = document.querySelector("main");
-  
-    // 2. get the list of parks
-    const parksList = main.querySelectorAll(".park-display");
-  
-    // 3. empty the main
-    main.innerHTML = "";
-  
-    // 4. create an array
-    const parksArray = Array.from(parksList);
-  
-    // 5. sort the array
-    parksArray.sort(sortByName);
-  
-    // 6. Insert each park into the DOM
-    parksArray.forEach((park) => {
-      main.appendChild(park);
-    });
+    //call render to now take the sorted arr and render each index(park)one by one in alphabetical order at this point
+    render();
 };
   
-  // function to handle the ratingSorter click
+// function to handle the ratingSorter click
 const ratingSorterClickHandler = (event) => {
     event.preventDefault();
-  
-    // 1.  get the main element
-    const main = document.querySelector("main");
-  
-    // 2. get the list of parks
-    const parksList = main.querySelectorAll(".park-display");
-  
-    // 3. empty the main
-    main.innerHTML = "";
-  
-    // 4. create an array
-    const parksArray = Array.from(parksList);
-  
-    // 5. sort the array
-    parksArray.sort(sortByRating);
-  
-    // 6. Insert each park into the DOM
-    parksArray.forEach((park) => {
-      main.appendChild(park);
-    });
+    //sort the array
+    parks.sort(sortByRating);
+    //let render do all the html work
+    render();
 };
   
 // the point where all the code starts
@@ -148,20 +140,30 @@ const main = () => {
     // add an event listener
     ratingSorter.addEventListener("click", ratingSorterClickHandler);
   
-    // select all the buttons for all the parks
-    const allBtns = document.querySelectorAll(".rate-button");
-  
-    // iterate the list of buttons and add an event handler to each
-    allBtns.forEach((btn) => {
-      btn.addEventListener("click", favoriteButtonClickHandler);
-    });
+    //BELOW doesn't work when elements are deleted from the DOM, once deleted, the listeners no longer work.. see line 148 for more info
+    // // select all the buttons for all the parks
+    // const allBtns = document.querySelectorAll(".rate-button");
+    // // iterate the list of buttons and add an event handler to each
+    // allBtns.forEach((btn) => {
+    //   btn.addEventListener("click", favoriteButtonClickHandler);
+    // });
+
+    //for elements that are deleted from the DOM, how can you make sure they're still listened to? 
+    //access an element that's never deleted like main or body 
+    //access main elem
+    const main = document.querySelector("main");
+
+    // Add event handler to the main
+    main.addEventListener("click", favoriteButtonClickHandler);
   
     // get the form element
     const form = document.querySelector("#park-form");
   
     // attach the submit handler
     form.addEventListener("submit", submitHandler);
+
+    render();
 };
   
-  // Add event listener for DOMContentLoaded
-  window.addEventListener("DOMContentLoaded", main);
+// Add event listener for DOMContentLoaded
+window.addEventListener("DOMContentLoaded", main);
